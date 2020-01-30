f21796f230af75c528d482c53f495a5c07ec4f94
StephenDividina <stephendividina28@gmail.com>
2020-01-30 19:37:59 +0800

    git init

---
+++ src/main/java/com/stephen/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.stephen.repository;
+
+import com.stephen.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByName(String username);
+}
